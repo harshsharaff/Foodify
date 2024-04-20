bfe0b7347937d0de3bff057865dd3f142af46c3b
Radhakrishna Vempati <vempatir@uci.edu>
2024-04-20 11:49:11 -0700

    webapp with chatbot

--- Foodify_La_hacks/Foodify_La_hacks.py
+++ Foodify_La_hacks/Foodify_La_hacks.py
@@ -4,31 +4,93 @@ from rxconfig import config
 
 import reflex as rx
 
-docs_url = "https://reflex.dev/docs/getting-started/introduction/"
-filename = f"{config.app_name}/{config.app_name}.py"
-
+# from chatapp import style
 
 class State(rx.State):
     """The app state."""
 
 
+shadow = "rgba(0, 0, 0, 0.15) 0px 2px 8px"
+chat_margin = "20%"
+message_style = dict(
+    padding="1em",
+    border_radius="5px",
+    margin_y="0.5em",
+    box_shadow=shadow,
+    max_width="30em",
+    display="inline-block",
+)
+
+# Set specific styles for questions and answers.
+question_style = message_style | dict(
+    margin_left=chat_margin,
+    background_color=rx.color("gray", 4),
+)
+answer_style = message_style | dict(
+    margin_right=chat_margin,
+    background_color=rx.color("accent", 8),
+)
+
+# Styles for the action bar.
+input_style = dict(
+    border_width="1px", padding="1em", box_shadow=shadow
+)
+button_style = dict(
+    background_color=rx.color("accent", 10),
+    box_shadow=shadow,
+)
+
+
+def qa(question: str, answer: str) -> rx.Component:
+    return rx.box(
+        rx.box(
+            rx.text(question, style=question_style),
+            text_align="right",
+        ),
+        rx.box(
+            rx.text(answer, style=answer_style),
+            text_align="left",
+        ),
+        margin_y="1em",
+    )
+
+
+def chat() -> rx.Component:
+    qa_pairs = [
+        (
+            "What is Reflex?",
+            "A way to build web apps in pure Python!",
+        ),
+        (
+            "What can I make with it?",
+            "Anything from a simple website to a complex web app!",
+        ),
+    ]
+    return rx.box(
+        *[
+            qa(question, answer)
+            for question, answer in qa_pairs
+        ]
+    )
+
+
+def action_bar() -> rx.Component:
+    return rx.hstack(
+        rx.input(
+            placeholder="Ask a question",
+            style=input_style,
+        ),
+        rx.button("Ask", style=button_style),
+    )
+
+
 def index() -> rx.Component:
     return rx.center(
-        rx.theme_panel(),
         rx.vstack(
-            rx.heading("Welcome to Reflex!", size="9"),
-            rx.text("Get started by editing ", rx.code(filename)),
-            rx.button(
-                "Check out our docs!",
-                on_click=lambda: rx.redirect(docs_url),
-                size="4",
-            ),
-            rx.logo(),
+            chat(),
+            action_bar(),
             align="center",
-            spacing="7",
-            font_size="2em",
-        ),
-        height="100vh",
+        )
     )
 
 
